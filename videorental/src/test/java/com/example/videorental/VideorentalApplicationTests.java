11578e23b4a9647aebe0b1ab458372a653b86510
Vic1617 <goldamen2007@gmail.com>
2026-02-24 19:22:35 +0100

    Initialise Spring Boot codebase

---
+++ videorental/src/test/java/com/example/videorental/VideorentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.videorental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideorentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
